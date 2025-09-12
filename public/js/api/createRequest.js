f23b0454b0e8b6a6e97ce45aa212f045cf03d698
Vladislav <vbura2000@gmail.com>
2025-09-12 09:21:09 +0300

    Подгрузка

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -1,7 +1,20 @@
-/**
- * Основная функция для совершения запросов
- * на сервер.
- * */
 const createRequest = (options = {}) => {
+  const xhr = new XMLHttpRequest();
+  if (options.method === "GET") {
+    xhr.open("GET", `${options.url}?${new URLSearchParams(options.data)}`);
+    xhr.send();
+  } else if (options.method === "POST") {
+    const formData = new FormData(options.data);
+    xhr.open("POST", options.url);
+    xhr.send(formData);
+  }
+  xhr.responseType = "json";
 
+  xhr.addEventListener("load", () => {
+    if (xhr.status === 200) {
+      options.callback(xhr.error, xhr.response);
+    } else if (xhr.status !== 200) {
+      options.callback(xhr.error);
+    }
+  });
 };
